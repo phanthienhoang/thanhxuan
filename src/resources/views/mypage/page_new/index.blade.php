3f0f889ad55c4e79177442ab2f5f1c8f7dda93c0
HoangPT <hoangpt@saisystem.vn>
2021-09-06 14:48:16 +0700

    db and news

--- src/resources/views/mypage/page_new/index.blade.php
+++ src/resources/views/mypage/page_new/index.blade.php
@@ -2,6 +2,18 @@
 @extends('mypage.layout_master.layout')
 
 @section('content')
+<div class="page-heading">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-8">
+          <div class="top-text header-text">
+            <!-- <h6>THANH XUÂN</h6> -->
+            <h2>ĐÁ QUÝ THANH XUÂN - CHUYÊN GIA PHONG THỦY</h2>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 <div class="container mt-5" style="min-height: 20px">
 </div>
 <div class="container mt-5">
@@ -9,33 +21,19 @@
         @foreach ($news as $new)
             <div class="col-md-6">
                 <div class="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
+                <div class="col-auto d-none d-lg-block">
+                        <img src="{{ asset('storage/'.$new->image) }}" alt="{{ $new->seo_title }}" srcset="{{ $new->seo_title }}">
+                    </div>
                     <div class="col p-4 d-flex flex-column position-static">
                         <strong class="d-inline-block mb-2 text-primary">{{ $new->title }}</strong>
                         <h3 class="mb-0">{{ $new->title }}</h3>
                         <div class="mb-1 text-muted">Nov 12</div>
-                        <p class="card-text mb-auto">{{ $new->content }}</p>
+                        <!-- <p class="card-text mb-auto">{!!$new->content!!}</p> -->
                         <a href="{{ $new->slug }}" class="stretched-link">Continue reading</a>
-                    </div>
-                    <div class="col-auto d-none d-lg-block">
-                        <img src="{{ asset('storage/'.$new->image) }}" alt="{{ $new->seo_title }}" srcset="{{ $new->seo_title }}">
                     </div>
                 </div>
             </div> 
         @endforeach
-        <div class="col-md-6">
-            <div class="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
-                <div class="col p-4 d-flex flex-column position-static">
-                    <strong class="d-inline-block mb-2 text-primary">World</strong>
-                    <h3 class="mb-0">Featured post</h3>
-                    <div class="mb-1 text-muted">Nov 12</div>
-                    <p class="card-text mb-auto">This is a wider card with supporting text below as a natural lead-in to additional content.</p>
-                    <a href="#" class="stretched-link">Continue reading</a>
-                </div>
-                <div class="col-auto d-none d-lg-block">
-                    <svg class="bd-placeholder-img" width="200" height="250" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
-                </div>
-            </div>
-        </div>
     </div>
 </div>
 @endsection
